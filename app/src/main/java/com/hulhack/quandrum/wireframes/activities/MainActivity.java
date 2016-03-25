52fc627a82662021768d82d87349beea4b0d57ba
Girish <girish95k@gmail.com>
2016-03-25 21:58:36 +0530

    volely

--- app/src/main/java/com/hulhack/quandrum/wireframes/activities/MainActivity.java
+++ app/src/main/java/com/hulhack/quandrum/wireframes/activities/MainActivity.java
@@ -1,19 +1,65 @@
 package com.hulhack.quandrum.wireframes.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.hulhack.quandrum.wireframes.R;
+import com.hulhack.quandrum.wireframes.utils.AppController;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String TAG = "Returned";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Hindustan Unilever");
+
+        // Tag used to cancel the request
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://65402a5f.ngrok.io/req_otp",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this,response,Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("number","9999999999");
+
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
     public void buttonClick(View view){
--- app/src/main/java/com/hulhack/quandrum/wireframes/fragments/sales/CalendarFragment.java
+++ app/src/main/java/com/hulhack/quandrum/wireframes/fragments/sales/CalendarFragment.java
@@ -1,5 +1,6 @@
 package com.hulhack.quandrum.wireframes.fragments.sales;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,17 +9,120 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.hulhack.quandrum.wireframes.R;
+import com.marcohc.robotocalendar.RobotoCalendarView;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 /**
  * Created by Ratan on 7/29/2015.
  */
-public class CalendarFragment extends Fragment {
+public class CalendarFragment extends Fragment implements RobotoCalendarView.RobotoCalendarListener {
 
+    private RobotoCalendarView robotoCalendarView;
+    private int currentMonthIndex;
+    private Calendar currentCalendar;
+
+    public String test = "[\n" +
+            "    {\n" +
+            "        \"token\": \"Aasd12197\",\n" +
+            "        \"Area\": \"NORTH GUJARAT SALES AREA\",\n" +
+            "        \"CUSTOMER_ID\": \"0000101008\",\n" +
+            "        \"MOC\": 4,\n" +
+            "        \"number\": \"8050199000\",\n" +
+            "        \"Customer_ID\": \"0000101008\",\n" +
+            "        \"Region\": \"GUJARAT \",\n" +
+            "        \"Date\": \"2016-03-21\",\n" +
+            "        \"RS_Name\": \"BHUPENDRA STORES\",\n" +
+            "        \"Channel\": \"GT\",\n" +
+            "        \"Business\": \"U1\",\n" +
+            "        \"Activity_Type\": \"SALES TARGET\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"token\": \"Aasd12197\",\n" +
+            "        \"Area\": \"NORTH GUJARAT SALES AREA\",\n" +
+            "        \"CUSTOMER_ID\": \"0000101008\",\n" +
+            "        \"MOC\": 4,\n" +
+            "        \"number\": \"8050199000\",\n" +
+            "        \"Customer_ID\": \"0000101008\",\n" +
+            "        \"Region\": \"GUJARAT \",\n" +
+            "        \"Date\": \"2016-03-21\",\n" +
+            "        \"RS_Name\": \"BHUPENDRA STORES\",\n" +
+            "        \"Channel\": \"GT\",\n" +
+            "        \"Business\": \"U1\",\n" +
+            "        \"Activity_Type\": \"SALES TARGET\"\n" +
+            "    }\n" +
+            "]\n";
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar,null);
+
+        View root = inflater.inflate(R.layout.fragment_calendar,null);
+        // Gets the calendar from the view
+        robotoCalendarView = (RobotoCalendarView) root.findViewById(R.id.robotoCalendarPicker);
+
+        // Set listener, in this case, the same activity
+        robotoCalendarView.setRobotoCalendarListener(this);
+
+        // Initialize the RobotoCalendarPicker with the current index and date
+        currentMonthIndex = 0;
+        currentCalendar = Calendar.getInstance(Locale.getDefault());
+
+        // Mark current day
+        robotoCalendarView.markDayAsCurrentDay(currentCalendar.getTime());
+
+        return root;
+    }
+/*
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+*/
+    @Override
+    public void onDateSelected(Date date) {
+
+        // Mark calendar day
+        robotoCalendarView.markDayAsSelectedDay(date);
+
+        // Mark that day with random colors
+        final Random random = new Random(System.currentTimeMillis());
+        final int style = random.nextInt(3);
+        switch (style) {
+            case 0:
+                robotoCalendarView.markFirstUnderlineWithStyle(RobotoCalendarView.BLUE_COLOR, date);
+                break;
+            case 1:
+                robotoCalendarView.markSecondUnderlineWithStyle(RobotoCalendarView.GREEN_COLOR, date);
+                break;
+            case 2:
+                robotoCalendarView.markFirstUnderlineWithStyle(RobotoCalendarView.RED_COLOR, date);
+                break;
+            default:
+                break;
+        }
     }
 
+    @Override
+    public void onRightButtonClick() {
+        currentMonthIndex++;
+        updateCalendar();
+    }
+
+    @Override
+    public void onLeftButtonClick() {
+        currentMonthIndex--;
+        updateCalendar();
+    }
+
+    private void updateCalendar() {
+        currentCalendar = Calendar.getInstance(Locale.getDefault());
+        currentCalendar.add(Calendar.MONTH, currentMonthIndex);
+        robotoCalendarView.initializeCalendar(currentCalendar);
+    }
 
 }
